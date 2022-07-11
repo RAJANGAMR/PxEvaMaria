5a12c8c8857d371bf437a3f7adc2110e6a20ac40
RAJANGAM R <85456269+RAJANGAMR@users.noreply.github.com>
2022-07-11 19:58:59 +0530

    Add files via upload

--- plugins/inline.py
+++ plugins/inline.py
@@ -19,17 +19,17 @@ async def inline_users(query: InlineQuery):
         return True
     return False
 
-
-@Client.on_inline_query(filters.user(AUTH_USERS) if AUTH_USERS else None)
+@Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
-
+    
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
                            switch_pm_text='okDa',
                            switch_pm_parameter="hehe")
         return
+
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
@@ -59,7 +59,7 @@ async def answer(bot, query):
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
                 f_caption=f_caption
@@ -88,10 +88,6 @@ async def answer(bot, query):
             pass
         except Exception as e:
             logging.exception(str(e))
-            await query.answer(results=[], is_personal=True,
-                           cache_time=cache_time,
-                           switch_pm_text=str(e)[:63],
-                           switch_pm_parameter="error")
     else:
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
